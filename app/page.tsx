f120eb7aa8eeb2823365927539a0d86f566e9a6c
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-16 09:45:08 -0300

    :lipstick: feat: Adicionando background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-   <main>
-    Home
+   <main className="w-screen h-screen relative">
+    <div className="flex items-center w-full h-full bg-cover bg-center" style={{backgroundImage: "url(/main-bg.webp)"}}>
+      <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+
+      </div>
+    </div>
    </main>
   );
 }
