6846d239cac3dc2c3a2d38be2caecf0190252fbd
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-20 20:44:51 -0300

    :construction: ci: Responsivel para todos os celulares

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,9 @@ export default {
   ],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '780px',
+      'sm': '360px',
+      '2sm': '412px',
+      'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
