0e8332d5d3df72f5c66f6bf7c5066fbca7eb94ef
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-21 17:06:59 -0300

    :bug: fix: Corrigindo erro de link não clicavel mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 z-[40] w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl'>
+    <div className='fixed top-0 z-[40] w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100]'>
         <div className='flex flex-row gap-3 items-center'>
-            <Link href="/" className='text-white text-[2rem] font-semibold md:text-[2.5rem] z-50' >André .</Link>
+            <Link href="/" className='text-white text-[2rem] font-semibold md:text-[2.5rem]' >André .</Link>
         </div>
 
         <div className='flex flex-row gap-3 mb-2'>
-            <Link href="https://www.linkedin.com/in/andr%C3%A9-carloscx/" target='_blank'><img src="/linkedin.svg" alt="Linkedin" /></Link>
+            <Link href="https://www.linkedin.com/in/andr%C3%A9-carloscx" target='_blank'><img src="/linkedin.svg" alt="Linkedin" /></Link>
             <Link href="https://github.com/AndreCarlos007" target='_blank'><img src="/github.svg" alt="Github" /></Link>
             <Link href="/"><img src="/discordnew.svg" alt="Discord" /></Link>
           
