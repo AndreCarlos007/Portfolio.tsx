92c01d9be62a5d1a53b239da506bdccbbab08a77
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-21 17:32:05 -0300

    :ok_hand: fix:  Revisão de código, backup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 z-[40] w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100]'>
+    <div className='fixed top-0 w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100]'>
         <div className='flex flex-row gap-3 items-center'>
             <Link href="/" className='text-white text-[2rem] font-semibold md:text-[2.5rem]' >André .</Link>
         </div>
