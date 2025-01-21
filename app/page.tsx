520fdd0599f0ffae8d5e7277048a9e05df2b215f
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-21 06:43:38 -0300

    :iphone: feat: Página inicial totalmente responsiva

--- app/page.tsx
+++ app/page.tsx
@@ -10,36 +10,34 @@ export default function Home() {
       className="flex  w-screen  h-screen  bg-cover bg-center"
       style={{ backgroundImage: "url(/main-bg.webp)" }}
     >
-      <div className="sm:pl-10 sm:pt-28 sm:max-w-[320px] md:pl-40 md:pt-36 flex flex-col gap-5 z-[50] ">
-        <h1 className="text-[50px] text-white hidden sm:block sm:text-[1.4rem] sm:font-semibold  ">
+      <div className="sm:pl-10 sm:pt-28 sm:max-w-[320px] md:max-w-[600px] md:pl-[5.3rem] md:pt-36 lg:pt-44 flex flex-col gap-5 z-[50] ">
+        <h1 className="text-[50px] text-white hidden sm:block sm:text-[1.4rem] sm:font-semibold md:text-[2rem] lg:text-[2.5rem] ">
           Make anything possible with
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-red-900">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
             {" "}
             Web Development
           </span>
         </h1>
-        <p className="text-gray-200 hidden sm:block  md:block">
+        <p className="text-gray-200 hidden sm:block md:block md:text-[1.3rem] lg:text-[1.4rem]">
         I create digital solutions that prioritize both aesthetics and usability. 
         </p>
         <div className="flex-col md:flex-row hidden md:flex gap-5">
           <Link
             href="/my-skills"
-            className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            className="px-6 py-2 font-medium bg-gradient-to-r from-purple-900 to-red-000 text-white w-fit transition-all shadow-[1px_1px_0px_white] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] rounded-md max-w-[200px]"
           >
             Learn more
           </Link>
           <Link
             href="/my-projects"
-            className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
+            className="px-6 py-2 font-medium bg-gradient-to-r from-purple-900 to-red-000 text-white w-fit transition-all shadow-[1px_1px_0px_white] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] rounded-md max-w-[200px]"
           >
-            <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
             My projects
           </Link>
           <Link
             href="/contacte-me"
-            className="rounded-[20px] group relative bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+            className="px-6 py-2 font-medium bg-gradient-to-r from-purple-900 to-red-000 text-white w-fit transition-all shadow-[1px_1px_0px_white] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] rounded-md max-w-[200px]"
           >
-            <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
             Contact
           </Link>
         </div>
@@ -65,7 +63,7 @@ export default function Home() {
         alt="horse"
         height={200}
         width={115}
-        className="absolute right-48 top-40"
+        className="absolute right-48 top-40 md:w-[350px] md:top-32 md:right-44 lg:top-4 lg:right-72"
       />
 
       <Image 
@@ -73,7 +71,7 @@ export default function Home() {
       alt="cliff" 
       width={310} 
       height={400}
-      className=""
+      className="md:w-[500px] lg:w-[600px] lg:h-[600px]"
       />
     </div>
 
