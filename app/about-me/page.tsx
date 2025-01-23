9b2d1430a1f9bcd6250fbd69fa84c9ca3ecaf975
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-23 17:57:52 -0300

    :sparkles: feat: Página sobre mim

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 import AboutSlider from '@/components/AboutSlider'
+import Link from 'next/link';
 import React from 'react'
 
 const page = () => {
   return (
      <div style={{backgroundImage: "url(/bg-2.jpg)" }}
         className='flex items-center justify-center w-screen h-screen bg-cover bg-center text-white gap-40'>
-            <div className="">
-                <h1>About me</h1>
+            <div className="max-w-[600px]">
+                <h1 className='text-[1.5rem] font-bold text-[#FFFFFF99]'>BIOGRAPHY</h1>
+                <div className='text-justify text-[0.9rem]'>
+                <p>Hello, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>André Carlos</span>, a 20-year-old Systems Analysis and Development student, and I am passionate about programming. 
+                    My journey began at the age of 17, during a technical IT course, where I was introduced to programming through languages like Portugol and Python. 
+                    That first contact ignited my love for technology, and since then, I have been dedicated to expanding my knowledge.</p>
+                <p className='pt-3'>I have come to know various technologies, frameworks, databases, and tools, constantly improving <Link href='/my-skills' className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'> my skill</Link> set. 
+                    I am a focused, detail-oriented, and dedicated individual, currently seeking an internship to gain practical experience and deepen my expertise.</p>
+                </div>
             </div>
             <div className='w-full pt-40 sm:pt-0  max-w-[300px] sm:max-w-[340px] md:pt-0 md:max-w-[350px]'>
                 <h1 className='font-bold pb-3 text-[2rem] '>My <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>Education.</span></h1>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Navigation from "@/components/ui/Navigation";
-import TransitionPage from "@/components/ui/TransitionPage";
-import TransitionBlocks from "@/components/ui/TransitionBlocks";
+// import TransitionPage from "@/components/ui/TransitionPage";
+// import TransitionBlocks from "@/components/ui/TransitionBlocks";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
