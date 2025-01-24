19df21fdb2612c722b8b472657d1170659149f86
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-24 05:42:28 -0300

    :lipstick: fix: Adicionando botão na página home, overflow hidden no mobile

--- app/contacte-me/page.tsx
+++ app/contacte-me/page.tsx
@@ -3,7 +3,7 @@ import {BsArrowRight} from 'react-icons/bs'
 const page = () => {
   return (
     <div style={{backgroundImage: "url(/bg-2.jpg)" }}
-    className='h-screen w-screen items-center justify-center flex bg-cover bg-center overflow-scroll md:overflow-hidden '>
+    className='h-screen w-screen items-center justify-center flex bg-cover bg-center overflow-scroll sm:overflow-hidden'>
         <div className='flex flex-col w-full max-w-[300px] sm:max-w-[340px] md:max-w-[600px] mt-72 mb-36 sm:mt-36 md:mt-0 md:mb-0'>
             <h2 className='text-white text-[2rem] font-bold mb-3 text-center'>
               Let&apos;s <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>connect.</span>
@@ -11,7 +11,7 @@ const page = () => {
             <form className=' text-white flex-1 flex flex-col gap-6 w-full mx-auto' action="">
                 <div className='flex gap-x-6 w-full'>
                     <input type="text" placeholder='name' name="name" className='w-full h-[40px] rounded-lg pl-6 capitalize bg-transparent border border-white/10 outline-none focus:ring-1 focus:ring-purple-800 placeholder:text-white/30 placeholder:font-light ' />
-                    <input type="email" placeholder='email' name="email" className='w-full h-[40px] rounded-lg pl-6 capitalize bg-transparent border border-white/10 outline-none focus:ring-1 focus:ring-purple-800 placeholder:text-white/30 placeholder:font-light' />
+                    <input type="email" placeholder='email' name="email" className='w-full h-[40px] rounded-lg pl-6 bg-transparent border border-white/10 outline-none focus:ring-1 focus:ring-purple-800 placeholder:text-white/30 placeholder:font-light' />
                 </div>
                 <input type="text" placeholder='subject' name="name" className='w-full h-[40px] rounded-lg pl-6 capitalize bg-transparent border border-white/10 outline-none focus:ring-1 focus:ring-purple-800 placeholder:text-white/30 placeholder:font-light' />
                 
