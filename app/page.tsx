14e168ba832fb0b9dd086f58628071957c5c7b70
Xiao-Dree <andrecarlos.oc@gmail.com>
2025-01-29 07:39:46 -0300

    :sparkles: feat: Adicionando preLoad nas fonts e fundo

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,20 @@ export default function Home() {
       className="flex w-screen  h-screen  bg-cover bg-center"
       style={{ backgroundImage: "url(/main-bg.webp)" }}
     >
+      <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sora:wght@100..800&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap"
+            as="style"
+            type="font/woff2"
+            crossOrigin='anonymous'
+          />
+          <link
+            rel="preload"
+            href="/main-bg.AVIF"
+            as="image"
+            type="image/avif"
+            sizes="100vw"
+          />
       <div className="sm:pl-10 sm:pt-28 sm:max-w-[320px] md:max-w-[600px] lg:max-w-[700px] md:pl-[5.3rem] md:pt-36  flex flex-col gap-5 z-[50] ">
         <h1 className="text-[50px] text-white hidden sm:block sm:text-[1.2rem] sm:font-semibold md:text-[2rem] lg:text-[2.5rem] ">
           Make anything possible with
