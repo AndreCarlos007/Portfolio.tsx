0756a05d6fbbce5a1d66e1043facea9575e2abee
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-01 14:50:24 -0300

    :lipstick: feat: Removendo bg secundário, Adicionando bg nas outras rotas

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='fixed top-0 w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100]'>
         <div className='flex flex-row gap-3 items-center'>
-            <Link href="/" className='text-white text-[2rem] font-semibold md:text-[2.5rem]' >André .</Link>
+            <Link href="/" className='text-white text-[1.5rem] sm:text-[2rem] font-semibold md:text-[2.5rem]' >André .</Link>
         </div>
 
         <div className='flex flex-row gap-3 mb-2'>
