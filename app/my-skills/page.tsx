fd80330bbddde39373496f0a5b44540a8eca911a
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-01 14:51:11 -0300

    :lipstick: fix: Estilização dos cards de skills

--- app/my-skills/page.tsx
+++ app/my-skills/page.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React from 'react'
-
 import { IconCloudDemo } from '@/components/ui/IconCloudDemo'
+import SkillsSlider from '@/components/SkillsSlider'
 
 const page = () => {
   return (
@@ -9,10 +10,11 @@ const page = () => {
         <div className="flex flex-col md:flex-row items-center gap-10  mt-80 mb-36 sm:mt-56 sm:mb-40 md:mb-0 md:mt-0">
           <div className='order-2 md:order-1 md:ml-20'>
           <h1 className='text-white font-bold text-[1.6rem] sm:text-[2rem]'>My <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 font-bold'>skills.</span></h1>
-          <p className='max-w-[270px] sm:max-w-[320px] md:max-w-[400px] text-[0.9rem] text-white text-justify'>I have knowledge in various web development tools and technologies and continuously strive to deepen my expertise. 
-            I&apos;m always learning, practicing, and staying updated in this ever-evolving field.</p>
+            <div className='max-w-[280px] sm:max-w-[320px]'>
+            <SkillsSlider />
+            </div>
           </div>
-          <div className='order-1 md:order-2'>
+          <div className='order-1 md:order-2 pt-36 sm: md:pt-0'>
           <IconCloudDemo />
           </div>
                 
--- components/AboutSlider.tsx
+++ components/AboutSlider.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Pagination, EffectCoverflow, Autoplay } from "swiper/modules"; // Importando o módulo EffectCoverflow
 import { FC } from "react";
 import { HiAcademicCap } from "react-icons/hi2";
 
@@ -31,19 +31,41 @@ const educationData: EducationCard[] = [
 const EducationSlider: FC = () => {
   return (
     <Swiper
-      spaceBetween={50}
+      direction="horizontal"
       pagination={{
         clickable: true,
       }}
-      modules={[Pagination]}
+      effect="coverflow" // Alterado para 'coverflow' para aplicar o efeito Coverflow
+      grabCursor={true} // Adiciona o cursor estilizado de arrastar
+      modules={[Pagination, EffectCoverflow, Autoplay]} // Incluindo o módulo EffectCoverflow
       className="h-[190px]"
+      coverflowEffect={{
+        rotate: 50, // Controle do ângulo de rotação
+        stretch: 0, // Controla a distorção de esticar os slides
+        depth: 100, // Profundidade do efeito de coverflow
+        modifier: 1, // Modificador de intensidade do efeito
+      }}
+      autoplay={{
+        delay: 3000, // Define o intervalo entre as trocas de slides
+        disableOnInteraction: false, // Permite o autoplay continuar após interação do usuário
+      }}
+      loop={true}
     >
       {educationData.map((education, index) => (
         <SwiperSlide key={index}>
-          <div className="p-6 bg-gradient-to-r from-blue-600 to-purple-500 rounded-lg shadow-lg text-white">
-            <h2 className="text-[1rem] font-bold mb-2 flex">{education.course}<p className="text-white"><HiAcademicCap /></p></h2>
-            <h3 className="text-[0.9rem] font-semibold mb-4">{education.institution}</h3>
-            <p className="text-[0.8rem] text-[#FFFFFF99] flex">{education.description}</p>
+          <div className="p-6 bg-gradient-to-r border-1 border-purple-400 bg-purple-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 rounded-lg shadow-lg text-white">
+            <h2 className="text-[1rem] font-bold mb-2 flex">
+              {education.course}
+              <p className="text-white">
+                <HiAcademicCap />
+              </p>
+            </h2>
+            <h3 className="text-[0.9rem] font-semibold mb-4">
+              {education.institution}
+            </h3>
+            <p className="text-[0.8rem] text-[#FFFFFF99] flex">
+              {education.description}
+            </p>
           </div>
         </SwiperSlide>
       ))}
