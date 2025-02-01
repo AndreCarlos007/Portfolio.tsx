4743e5836bfd121560e4474e9f725030582f8f11
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-01 14:51:36 -0300

    :bug: fix: Preload

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -14,7 +14,6 @@ const Page: React.FC = () => {
     <div 
       style={{backgroundImage: "url(/bg-2.AVIF)" }} // Adiciona a imagem de fundo
       className='flex flex-col md:flex-row overflow-scroll md:overflow-hidden items-center justify-center w-screen h-screen bg-cover bg-center text-white gap-10 sm:gap-10 md:gap-8 lg:gap-32'>
-      
         <link
             rel="preload"
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sora:wght@100..800&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap"
