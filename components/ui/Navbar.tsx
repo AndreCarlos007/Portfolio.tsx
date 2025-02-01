ccce855ab3a4c541bd0e5d8bec63d4f4e59297b8
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-01 14:51:38 -0300

    :lipstick: fix: Mudanças na estilização, removendo buttons do home, hover na navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -5,15 +5,24 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100] '>
+    <div className='fixed top-0 w-full h-[90px] gap-2 lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl z-[100] '>
         <div className='flex flex-row gap-3 items-center'>
-            <Link href="/" className='text-white text-[1.5rem] sm:text-[2rem] font-semibold md:text-[2.5rem]' >André .</Link>
+            <Link href="/" className='text-white text-[1.5rem] sm:text-[2rem] font-semibold md:text-[2.5rem]' >André.</Link>
         </div>
 
         <div className='flex flex-row gap-3 mb-2'>
-            <Link href="https://www.linkedin.com/in/andr%C3%A9-carloscx" rel="noopener noreferrer" target='_blank'><Image width={40} height={40} loading='lazy' src="/linkedin.svg" alt="Linkedin" /></Link>
-            <Link href="https://github.com/AndreCarlos007" rel="noopener noreferrer" target='_blank'><Image width={40} height={40} loading='lazy'  src="/github.svg" alt="Github" /></Link>
-            <Link href="/curriculoAndre.pdf" rel="noopener noreferrer" target='_blank' ><Image width={40} height={40} loading='lazy'   src="/curriculum.AVIF" alt="Discord" /></Link>
+          <div className='bg-gray-400 p-[0.50rem] rounded-full hover:bg-purple-950 transition-all duration-150'>
+          <Link href="https://www.linkedin.com/in/andr%C3%A9-carloscx" rel="noopener noreferrer" target='_blank'><Image width={40} height={40} loading='lazy' src="/linkedin.svg" alt="Linkedin" /></Link>
+          </div>
+          
+          <div className='bg-gray-400 p-[0.50rem] rounded-full hover:bg-purple-950 transition-all duration-150'>
+          <Link href="https://github.com/AndreCarlos007" rel="noopener noreferrer" target='_blank'><Image width={40} height={40} loading='lazy'  src="/github.svg" alt="Github" /></Link>
+          </div>
+            
+          <div className='bg-gray-400 p-[0.50rem] rounded-full hover:bg-purple-950 transition-all duration-150'>
+          <Link href="/curriculoAndre.pdf" rel="noopener noreferrer" target='_blank' ><Image width={40} height={40} loading='lazy'   src="/curriculum.AVIF" alt="Discord" /></Link>
+          </div>
+            
           
         </div>
     </div>
