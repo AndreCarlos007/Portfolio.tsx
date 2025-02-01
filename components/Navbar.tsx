0a99d95c119ad889bd91bf2a83481aab4b252689
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-01 14:50:21 -0300

    :bug: fix: Corrigindo erro de link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='fixed top-0 z-[40] w-full h-[90px] lg:h-[90px] bg-[#0000008a] flex justify-between items-center px-10 md:px-20 max-w-screen-2xl rounded-r-3xl'>
         <div className='flex flex-row gap-3 items-center'>
-
-            <a className='text-white text-[2rem] font-semibold md:text-[2.5rem] z-50' href="/">André .</a>
+            <Link href="/" className='text-white text-[2rem] font-semibold md:text-[2.5rem] z-50' >André .</Link>
         </div>
 
         <div className='flex flex-row gap-3 mb-2'>
